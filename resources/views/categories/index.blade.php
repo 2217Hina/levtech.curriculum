c36ba6aba4f2d9b4a6c955277f1ef5476d25b304
2217Hina <hina.funatsu@gmail.com>
2023-04-01 17:41:39 +0900

    Breeze2
    ;

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
+<x-app-layout>
+    <x-slot name="header">
+        　（ヘッダー名）
+    </x-slot>
         <h1>Category</h1>
         @foreach($posts as $post)
         <h2 class='title'>
             <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
         </h2>
         @endforeach
-    </body>
-</html>
+    </x-app-layout>
